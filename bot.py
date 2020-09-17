677bd3c6bfbd01a47ed4c4cb3e657e504e6fefe4
yerassylnurlanov <47392155+yerassylnurlanov@users.noreply.github.com>
2020-09-17 23:36:22 +0600

    from home

--- bot.py
+++ bot.py
@@ -13,24 +13,64 @@ global basket
 basket = []
 
 
-
 @bot.message_handler(commands=['start'])
-def any_msg(message):
-    url='https://img.povar.ru/uploads/e6/32/6e/0e/vanilnii_latte-645351.JPG'
-    keyboard = types.InlineKeyboardMarkup()
-    callback_button = types.InlineKeyboardButton(text="Добавить в корзину", callback_data="addToCartLatte")
-    keyboard.add(callback_button)
-    bot.send_photo(message.chat.id, url,caption ='Латте',reply_markup=keyboard)
+def sendWelcomeMessage(message):
+    msg = "Добрый день, "+message.from_user.first_name
+    markup = types.ReplyKeyboardMarkup()
+    okButton = types.KeyboardButton('Начать заказывать')
+    markup.add(okButton)
+    bot.send_message(message.chat.id, msg,reply_markup=markup)
+
+
+@bot.message_handler(func=lambda m: True) # Handler for messages
+def send_messages(message):
+    if(message.text == 'Начать заказывать'):
+        markup = types.ReplyKeyboardMarkup(row_width=3)
+        menuBtn = types.KeyboardButton('Меню')
+        basketBtn = types.KeyboardButton('Корзина')
+        orderBtn = types.KeyboardButton('Оформить заказ')
+        aboutBtn = types.KeyboardButton('О нас')
+        myOrdersBtn = types.KeyboardButton('Мои заказы')
+        callBtn = types.KeyboardButton('Связаться с менеджером')
+        markup.add(menuBtn)
+        markup.add(basketBtn)
+        markup.add(orderBtn)
+        markup.add(myOrdersBtn)
+        markup.add(callBtn)
+        bot.send_message(message.chat.id, 'Отлично! Приятного пользования!',reply_markup=markup)
+    elif(message.text =='Меню'):
+        url='https://img.povar.ru/uploads/e6/32/6e/0e/vanilnii_latte-645351.JPG'
+        keyboard = types.InlineKeyboardMarkup()
+        callback_button = types.InlineKeyboardButton(text="Добавить в корзину", callback_data="addToCartLatte")
+        keyboard.add(callback_button)
+        bot.send_photo(message.chat.id, url,caption ='Латте',reply_markup=keyboard)
+    elif(message.text == 'Корзина'):
+        print(basket)
+        
+@bot.callback_query_handler(func=lambda call: True) # Handler for queries
+def menu(call):
+    if call.data=="addToCartLatte":
+        small = types.InlineKeyboardButton(text="300 Мл", callback_data="small_latte")
+        medium = types.InlineKeyboardButton(text="400 Мл", callback_data="medium_latte")
+        large = types.InlineKeyboardButton(text="500 Мл", callback_data="large_latte")
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(small)
+        keyboard.add(medium)
+        keyboard.add(large)
+        bot.edit_message_caption(chat_id=call.message.chat.id, message_id=call.message.message_id, caption='Латте \nВыберите размер:\n',reply_markup = keyboard)
+    if call.data == "small_latte":
+        amount = types.InlineKeyboardMarkup()
+        for i in range(1, 10):
+            amount.add(types.InlineKeyboardButton(text=i, callback_data=i))
+        bot.edit_message_caption(chat_id=call.message.chat.id, message_id=call.message.message_id, caption='Латте \nВыберите количество:\n',reply_markup = amount)
+        
+if __name__ == '__main__':
+    bot.infinity_polling()
 
 
     
-@bot.callback_query_handler(func=lambda call: True)
-def longname(call):
-    bot.edit_message_caption(chat_id=call.message.chat.id,message_id=call.message.message_id,caption='Латте добавлен в корзину')
-    if call.data == "addToCartLatte":
-        basket.append('Латтeeeе')
-    print(basket)
 
 
-if __name__ == '__main__':
-    bot.infinity_polling()
+
+
+
